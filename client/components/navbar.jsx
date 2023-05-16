f820bfe4a674a40814f4efaa1cccf84f8ba00dc1
Parker Siu <parker.siu@gmail.com>
2023-05-16 19:24:27 +0000

    Updated auth conditional render logic.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import RegisterModal from './register-modal';
 
 export default function Navbar(props) {
@@ -6,6 +6,7 @@ export default function Navbar(props) {
   const [type, setType] = useState('');
   // eslint-disable-next-line
   const [userToken, setUserToken] = useState();
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const handleShowRegister = () => {
     setShow(true);
@@ -22,23 +23,26 @@ export default function Navbar(props) {
     setUserToken(userToken);
   };
 
-  const getToken = () => {
-    const tokenString = sessionStorage.getItem('token');
-    const userToken = JSON.parse(tokenString);
-    return userToken;
-  };
-
-  const token = getToken();
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+    const parsed = JSON.parse(token);
+    if (parsed) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, []);
 
   const logout = () => {
     sessionStorage.clear();
     setUserToken();
     props.setUser(null);
+    setLoggedIn(false);
   };
 
   return (
     <div>
-      <RegisterModal show={show} type={type} onClose={handleClose} setToken={setToken} setUser={props.setUser} />
+      <RegisterModal show={show} type={type} onClose={handleClose} setToken={setToken} setUser={props.setUser} setLoggedIn={setLoggedIn} />
       <nav className="navbar navbar-expand-lg navbar-dark">
         <div className="container-fluid">
           <a className="navbar-brand text-white" href="#">Milestone</a>
@@ -57,7 +61,7 @@ export default function Navbar(props) {
                 <a className="nav-link nav-text" href='#about'>About</a>
               </li>
             </ul>
-            {token
+            {loggedIn
               ? <button className='btn btn-outline-primary nav-button p-2' type='button' onClick={logout}>Sign Out</button>
               : <div>
                 <button className="btn btn-outline-primary me-2 nav-button" type="button" onClick={handleShowRegister}>Register</button>
